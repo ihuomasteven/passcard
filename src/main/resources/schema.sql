eed68de986d4d51ba9d8c5f6bd139940ffed23a1
emmysteven <hello@emmysteven.com>
2023-03-18 21:04:15 +0100

    create table for users

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table if not exists users (
+    id serial primary key,
+    username varchar(25) not null,
+    password varchar(255) not null,
+    email varchar(100) not null unique,
+    roles char(6) not null
+);
