c98ebcc131341a00a8cb9b1d733ed4d0e8d2e34d
emmysteven <hello@emmysteven.com>
2023-03-18 21:10:58 +0100

    add user entity

---
+++ src/main/java/passcard/domain/entity/User.java
@@ -0,0 +1,74 @@
+package passcard.domain.entity;
+
+import passcard.domain.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("users")
+public class User implements UserDetails {
+    @Id
+    private Long id;
+
+    @NotNull
+    @Size(min = 5, max = 20)
+    private String username;
+
+    @NotNull
+    @Size(min = 8, max = 20)
+    private String password;
+
+    @NotNull
+    @Email
+    private String email;
+
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() { return username;}
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() { return true; }
+}
