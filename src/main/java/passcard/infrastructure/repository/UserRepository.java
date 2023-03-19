91da6c811520f6a3433bfe194f318a8675accf24
emmysteven <hello@emmysteven.com>
2023-03-19 05:23:23 +0100

    add repository for user entity

---
+++ src/main/java/passcard/infrastructure/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package passcard.infrastructure.repository;
+
+import passcard.domain.entity.User;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends ReactiveCrudRepository<User, String> {
+
+    Mono<User> findByUsername(String username);
+
+}
