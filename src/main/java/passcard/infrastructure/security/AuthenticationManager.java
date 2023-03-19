7d96a0345f294d188f7de60a371f330d676fb6fb
emmysteven <hello@emmysteven.com>
2023-03-19 05:27:11 +0100

    add an authentication manager

---
+++ src/main/java/passcard/infrastructure/security/AuthenticationManager.java
@@ -0,0 +1,39 @@
+package passcard.infrastructure.security;
+
+import io.jsonwebtoken.Claims;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@AllArgsConstructor
+public class AuthenticationManager implements ReactiveAuthenticationManager {
+
+    private TokenProvider tokenProvider;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Mono<Authentication> authenticate(Authentication authentication) {
+        String token = authentication.getCredentials().toString();
+        String username = tokenProvider.getUsernameFromToken(token);
+        return Mono.just(tokenProvider.validateToken(token))
+                .filter(valid -> valid)
+                .switchIfEmpty(Mono.empty())
+                .map(valid -> {
+                    Claims claims = tokenProvider.getClaims(token);
+                    List<String> rolesMap = claims.get("role", List.class);
+                    return new UsernamePasswordAuthenticationToken(
+                            username,
+                            null,
+                            rolesMap.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList())
+                    );
+                });
+    }
+}
