bb3b53c7493f67597bf090016f85791216bb4470
emmysteven <hello@emmysteven.com>
2023-03-19 05:33:00 +0100

    add utility class that handles jwt token

---
+++ src/main/java/passcard/infrastructure/security/TokenProvider.java
@@ -0,0 +1,117 @@
+package passcard.infrastructure.security;
+
+import passcard.infrastructure.config.JwtConfig;
+import io.jsonwebtoken.*;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import passcard.domain.entity.User;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.time.Instant;
+import java.util.Date;
+
+
+@Slf4j
+@Component
+public class TokenProvider {
+
+    private SecretKeySpec key;
+    private final JwtConfig jwtConfig;
+
+    public TokenProvider(JwtConfig jwtConfig) {
+        this.jwtConfig = jwtConfig;
+    }
+
+    @PostConstruct
+    protected void init() {
+        byte[] keyBytes = jwtConfig.getSecret().getBytes();
+        key = new SecretKeySpec(keyBytes, "HmacSHA512");
+    }
+
+    public String generateToken(User user) {
+        return generateTokenFromUserId(user.getId());
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public String generateTokenFromUserId(Long userId) {
+        Instant expiryDate = Instant.now().plusMillis(jwtConfig.getExpiration());
+        return Jwts.builder()
+                .setSubject(Long.toString(userId))
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(expiryDate))
+                .signWith(key)
+                .compact();
+    }
+
+    public Long getUserIdFromJWT(String token) {
+        Jws<Claims> jwsClaims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
+
+        return Long.valueOf(jwsClaims.getBody().getSubject());
+    }
+
+    public String resolveToken(ServerWebExchange req) {
+        String bearerToken = req.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public Authentication getAuthentication(String token) {
+        String userName = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+        User user = new User();
+        return new UsernamePasswordAuthenticationToken(userName, "", user.getAuthorities());
+    }
+
+    public Long getExpiryDuration() {
+        return jwtConfig.getExpiration();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SecurityException ex) {
+            log.error("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            log.error("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty.");
+        }
+        return false;
+    }
+}
