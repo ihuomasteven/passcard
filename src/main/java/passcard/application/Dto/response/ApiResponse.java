01c57a68424045a371afb594eec05f0c62c5cf09
emmysteven <hello@emmysteven.com>
2023-03-19 05:17:15 +0100

    add DTO to handle api response

---
+++ src/main/java/passcard/application/Dto/response/ApiResponse.java
@@ -0,0 +1,18 @@
+package passcard.application.Dto.response;
+
+import lombok.*;
+
+@Data
+public class ApiResponse {
+
+    private String data;
+    private Boolean success;
+
+    public ApiResponse() {
+    }
+
+    public ApiResponse(String data, Boolean success) {
+        this.data = data;
+        this.success = success;
+    }
+}
