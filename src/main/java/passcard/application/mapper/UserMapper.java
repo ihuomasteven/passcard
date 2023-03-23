7bed76bb29a20ddc25774147284d6b113afbf4b2
emmysteven <hello@emmysteven.com>
2023-03-23 05:27:00 +0100

    add mappings for user entity

---
+++ src/main/java/passcard/application/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package passcard.application.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import passcard.application.Dto.request.LoginDto;
+import passcard.domain.entity.User;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    UserMapper MAPPER = Mappers.getMapper(UserMapper.class);
+
+    LoginDto mapToUserDto(User user);
+
+    @Mapping(target = "authorities", ignore = true)
+    User mapToUser(LoginDto loginDto);
+}
