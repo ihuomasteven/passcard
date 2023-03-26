b6a19db57255e573095ab521f3d5da3707623cba
emmysteven <hello@emmysteven.com>
2023-03-26 06:55:26 +0100

    feat: use conventional names for columns

--- src/main/java/passcard/domain/common/BaseEntity.java
+++ src/main/java/passcard/domain/common/BaseEntity.java
@@ -3,6 +3,7 @@ package passcard.domain.common;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.springframework.data.annotation.*;
+import org.springframework.data.relational.core.mapping.Column;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -17,18 +18,22 @@ public class BaseEntity implements Serializable {
 
     @JsonIgnore
     @CreatedBy
+    @Column("created_by")
     private String createdBy;
 
     @JsonIgnore
     @CreatedDate
+    @Column("created_dt")
     private String createdDate;
 
     @JsonIgnore
     @LastModifiedBy
+    @Column("updated_by")
     private String updatedBy;
 
     @JsonIgnore
     @LastModifiedDate
+    @Column("updated_dt")
     private String updatedDate;
 
     @JsonIgnore
