5e16e44bb407b2f9fe9dd1691eb6287da1ec83b5
emmysteven <hello@emmysteven.com>
2023-03-26 06:53:21 +0100

    feat: add columns for auditing

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,5 +3,10 @@ create table if not exists users (
     username varchar(25) not null,
     password varchar(255) not null,
     email varchar(100) not null unique,
-    roles char(6) not null
+    roles char(6) not null,
+    created_by varchar(50),
+    created_dt timestamp,
+    updated_by varchar(50),
+    updated_dt timestamp,
+    version integer not null default 0
 );
