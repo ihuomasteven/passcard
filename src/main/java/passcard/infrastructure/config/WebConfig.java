2e1b2d721f7782c9c7aab6c03699bc0151a65660
emmysteven <hello@emmysteven.com>
2023-03-31 03:13:49 +0100

    add remove bean for UserRegisteredEvent

--- src/main/java/passcard/infrastructure/config/WebConfig.java
+++ src/main/java/passcard/infrastructure/config/WebConfig.java
@@ -1,15 +1,8 @@
 package passcard.infrastructure.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import passcard.domain.event.UserRegisteredEvent;
-import reactor.core.publisher.Sinks;
 
 @Configuration
 public class WebConfig {
-
-    @Bean
-    public Sinks.Many<UserRegisteredEvent> userRegisteredEventSinks() {
-        return Sinks.many().multicast().onBackpressureBuffer();
-    }
+    
 }
