74fe0de202db03cf456ec2d7530c7ade2d1914f8
emmysteven <hello@emmysteven.com>
2023-03-31 04:59:18 +0100

    refactor: remove validations as there are no need for them

--- src/main/java/passcard/application/Dto/request/LoginDto.java
+++ src/main/java/passcard/application/Dto/request/LoginDto.java
@@ -1,15 +1,5 @@
 package passcard.application.Dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
 import java.io.Serializable;
 
-public record LoginDto(
-    @Size(min = 3, max = 30)
-    @NotBlank(message = "Please fill in your username or email")
-    String username,
-
-    @NotBlank(message = "Login password cannot be blank")
-    String password
-) implements Serializable {}
+public record LoginDto(String username, String password) implements Serializable {}
