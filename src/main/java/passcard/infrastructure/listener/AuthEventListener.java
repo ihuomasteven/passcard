dc69df273f697ae7815a2f0a751b36041f694458
emmysteven <hello@emmysteven.com>
2023-04-01 19:38:51 +0100

    send a mail when a user logs in

--- src/main/java/passcard/infrastructure/listener/AuthEventListener.java
+++ src/main/java/passcard/infrastructure/listener/AuthEventListener.java
@@ -1,22 +1,34 @@
 package passcard.infrastructure.listener;
 
+import freemarker.template.TemplateException;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import passcard.domain.event.SignedInEvent;
 import passcard.domain.event.SignedUpEvent;
+import passcard.infrastructure.service.MailService;
 
+import java.io.IOException;
+
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class AuthEventListener {
+
+    private final MailService mailService;
 
     @EventListener
     public void handleSignedUpEvent(SignedUpEvent event) {
 
-        System.out.println("User registered: " + event.user().getEmail());
+        log.info("User registered: " + event.user().getEmail());
     }
 
     @EventListener
-    public void handleSignedInEvent(SignedInEvent event) {
+    public void handleSignedInEvent(SignedInEvent event) throws TemplateException, MessagingException, IOException {
 
-        System.out.println("User signed in successfully: " + event.user().getUsername());
+        mailService.sendAccountActivityEmail(event.user().getEmail(), "Login", "Successful");
+        log.info("User signed in successfully: " + event.user().getUsername());
     }
 }
